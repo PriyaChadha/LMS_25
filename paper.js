a2141bd7e424321215af1193b63ef96257a82c57
PriyaChadha <71523562+PriyaChadha@users.noreply.github.com>
2021-05-05 04:58:16 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -27,7 +27,7 @@ class paper
 			translate(paperpos.x, paperpos.y);
 			rectMode(CENTER)
 			//strokeWeight(4);
-			fill(255,0,255)
+			fill('#e32636')
 			imageMode(CENTER);
 			image(this.image, 0,0,this.r, this.r)
 			//ellipse(0,0,this.r, this.r);
